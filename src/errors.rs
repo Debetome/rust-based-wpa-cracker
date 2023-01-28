3947149b24cb4dc9f1dfa58e87eca33d80d43543
Debetome <magregory@tutanota.com>
2023-01-28 02:45:58 -0500

    Adding custom error types

---
+++ src/errors.rs
@@ -0,0 +1,44 @@
+use std::error::Error;
+use std::fmt;
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum ConfigErrorType {
+    InvalidArgument,
+    InvalidValue,
+    IncorrectValue,
+    MissingArgument
+}
+
+impl fmt::Display for ConfigErrorType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::InvalidArgument => write!(f, "Invalid Argument"),
+            Self::InvalidValue => write!(f, "Invalid Value"),
+            Self::IncorrectValue => write!(f, "Incorrect Value"),
+            Self::MissingArgument => write!(f, "Missing Argument")
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub struct ConfigError {
+    pub desc: &'static str,
+    pub code: ConfigErrorType,
+    pub detail: Option<String>
+}
+
+impl fmt::Display for ConfigError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.detail {
+            Some(ref detail) => f.write_fmt(
+                format_args!("{} - {} ({})", self.desc, detail, self.code)),
+            None => f.write_fmt(format_args!("{} ({})", self.desc, self.code))
+        }
+    }
+}
+
+impl Error for ConfigError {
+    fn description(&self) -> &'static str {
+        self.desc
+    }
+}
